7f05bfe130a1b3af8a7a32ad31a02816becd7dd9
aaryanthusoo <thusooaaryan@gmail.com>
2026-06-20 19:17:51 -0400

    Removed unneeded import

--- scripts/real/real_cnn_model_evaluation.py
+++ scripts/real/real_cnn_model_evaluation.py
@@ -8,7 +8,6 @@ from matplotlib.ticker import MaxNLocator
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
 sys.path.insert(0, str(PROJECT_ROOT))
 
-from tensorflow import keras
 import json
 
 from scripts.helpers.yaml_reading import load_params
