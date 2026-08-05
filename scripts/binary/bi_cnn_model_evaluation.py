013d6129b95478dc76fa5cccdf10483b4077f39b
aaryanthusoo <thusooaaryan@gmail.com>
2026-08-05 16:29:36 +0100

    Updates

--- scripts/binary/bi_cnn_model_evaluation.py
+++ scripts/binary/bi_cnn_model_evaluation.py
@@ -24,7 +24,6 @@ YAML_FILE = PROJECT_ROOT / "configs"/ "bi_evaluation_params.yaml"
 DATA_DIR = PROJECT_ROOT / "data" / "binary_model"
 MODEL_PATH = PROJECT_ROOT / "models" / "binary_model.keras"
 RESULTS_DIR = PROJECT_ROOT / "results" / "binary"
-PAPER_DIR = PROJECT_ROOT / "paper" / "figures"
 
 
 def data_loading():
@@ -48,10 +47,10 @@ def plot_training_history(path):
         loss = params["loss"]
         val_loss = params["val_loss"]
 
-    epochs = np.arange(1, len(accuracy) + 1)
+    epochs = np.arange(0, len(accuracy))
 
     # Loss plot
-    plt.figure(figsize=(12, 3))
+    plt.figure(figsize=(12, 4))
     plt.plot(epochs, loss, label="Training Loss")
     plt.plot(epochs, val_loss, label="Validation Loss")
 
@@ -62,10 +61,10 @@ def plot_training_history(path):
     plt.ylabel("Loss")
     plt.title("Training and Validation Loss")
     plt.legend()
-    plt.savefig(PAPER_DIR / "plots" / "bi_training_loss_history.png")
+    plt.savefig(RESULTS_DIR / "plots" / "training_loss_history.png")
 
     # Accuracy plot
-    plt.figure(figsize=(12, 3))
+    plt.figure(figsize=(12, 4))
     plt.plot(epochs, accuracy, label="Training Accuracy")
     plt.plot(epochs, val_accuracy, label="Validation Accuracy")
 
@@ -74,9 +73,10 @@ def plot_training_history(path):
 
     plt.xlabel("Epoch")
     plt.ylabel("Accuracy")
+    #plt.ylim(accuracy[0] - 0.05, accuracy[-1] + 0.05)
     plt.title("Training and Validation Accuracy")
     plt.legend()
-    plt.savefig(PAPER_DIR / "plots" / "bi_training_accuracy_history.png")
+    plt.savefig(RESULTS_DIR / "plots" / "training_accuracy_history.png")
 
 def plot_confusion_matrix(all_true, all_preds):
     class_names = ["Eclipsing Binary", "Transit"]
@@ -123,7 +123,7 @@ def plot_confusion_matrix(all_true, all_preds):
     colorbar.set_label("Fraction of True Class", rotation=270, labelpad=18)
 
     fig.tight_layout()
-    fig.savefig(PAPER_DIR / "plots" / "bi_confusion_matrix.png", dpi=200, bbox_inches="tight")
+    fig.savefig(RESULTS_DIR / "plots" / "confusion_matrix.png", dpi=200, bbox_inches="tight")
     plt.close(fig)
 
 def calculate_classification_metrics(
@@ -211,7 +211,7 @@ def plot_correct_incorrect_confidence(
     plt.title("Prediction Confidence: Correct vs Incorrect")
     plt.legend()
     plt.tight_layout()
-    plt.savefig(PAPER_DIR / "plots" / "bi_confidence_correct_vs_incorrect.png", dpi=200)
+    plt.savefig(results_dir / "confidence_correct_vs_incorrect.png", dpi=200)
     plt.close()
 
 
